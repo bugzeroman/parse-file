4290146e163f3bb338bbb7d4ed5d4e7d23475331
yuwen <yuwen3@asiainfo.com>
2020-12-16 16:45:55 +0800

    使用JAXB提供的方法更方便的解析和生成XML文件

---
+++ xml-file/xml-java/src/main/java/javax/xml/bind/JAXBContextParseXML.java
@@ -0,0 +1,54 @@
+package javax.xml.bind;
+
+import java.io.File;
+
+/**
+ * 使用JAXBContext上下文对象，通过实体对象直接生成XML，以及把XML直接解析成对应的实体类实例
+ *
+ */
+public class JAXBContextParseXML {
+    public static final String FILE_NAME = "src/main/resources/person.xml";
+
+    public static void main(String[] args) throws Exception {
+        generateXML();
+        generateBean();
+    }
+
+    /**
+     * 通过实体对象直接生成XML
+     */
+    public static void generateXML() throws Exception {
+        Person person = new Person("yuwen", "男", "南京");
+
+        File file = new File(FILE_NAME);
+        // 根据Person类生成上下文对象
+        JAXBContext jc = JAXBContext.newInstance(Person.class);
+        // 从上下文中获取Marshaller对象，用于把bean转换为xml
+        Marshaller ma = jc.createMarshaller();
+        // 以下是为生成xml做的一些配置
+        // 格式化输出，即按标签自动换行，否则就是一行输出
+        ma.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        // 设置编码（默认编码就是utf-8）
+        ma.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+        // 是否省略xml头信息，默认不省略（false）
+        ma.setProperty(Marshaller.JAXB_FRAGMENT, false);
+
+        // 编组，把Person对象转换为xml
+        ma.marshal(person, file);
+        System.out.println("generateXML=" + file.getName());
+    }
+
+    /**
+     * 把XML直接解析成对应的实体类实例
+     */
+    public static void generateBean() throws Exception {
+        // 从上下文中获取Unmarshaller对象
+        JAXBContext jc = JAXBContext.newInstance(Person.class);
+        Unmarshaller uma = jc.createUnmarshaller();
+
+        // 把XML文件解析为Person对象
+        File file = new File(FILE_NAME);
+        Person person = (Person) uma.unmarshal(file);
+        System.out.println(person);
+    }
+}
--- xml-file/xml-java/src/main/java/javax/xml/bind/JAXBParseXML.java
+++ xml-file/xml-java/src/main/java/javax/xml/bind/JAXBParseXML.java
@@ -3,7 +3,7 @@ package javax.xml.bind;
 import java.io.File;
 
 /**
- * 通过实体对象直接生成XML，以及把XML直接解析成对应的实体类实例
+ * 使用JAXB的静态方法（推荐），通过实体对象直接生成XML，以及把XML直接解析成对应的实体类实例
  *
  */
 public class JAXBParseXML {
@@ -19,36 +19,17 @@ public class JAXBParseXML {
      */
     public static void generateXML() throws Exception {
         Person person = new Person("yuwen", "男", "南京");
-
         File file = new File(FILE_NAME);
-        // 根据Person类生成上下文对象
-        JAXBContext jc = JAXBContext.newInstance(Person.class);
-        // 从上下文中获取Marshaller对象，用于把bean转换为xml
-        Marshaller ma = jc.createMarshaller();
-        // 以下是为生成xml做的一些配置
-        // 格式化输出，即按标签自动换行，否则就是一行输出
-        ma.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        // 设置编码（默认编码就是utf-8）
-        ma.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-        // 是否省略xml头信息，默认不省略（false）
-        ma.setProperty(Marshaller.JAXB_FRAGMENT, false);
-
-        // 编组，把Person对象转换为xml
-        ma.marshal(person, file);
-        System.out.println("generateXML=" + file.getName());
+        JAXB.marshal(person, file);
+        System.out.println("generateXML=" + file.getAbsolutePath());
     }
 
     /**
      * 把XML直接解析成对应的实体类实例
      */
     public static void generateBean() throws Exception {
-        // 从上下文中获取Unmarshaller对象
-        JAXBContext jc = JAXBContext.newInstance(Person.class);
-        Unmarshaller uma = jc.createUnmarshaller();
-
-        // 把XML文件解析为Person对象
         File file = new File(FILE_NAME);
-        Person person = (Person) uma.unmarshal(file);
+        Person person = JAXB.unmarshal(file, Person.class);
         System.out.println(person);
     }
 }
